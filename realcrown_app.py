bbba3965f53e5e561c89f2d0ca5fe9b0371f9a06
RealCrownInitiative <realcrowninitiative@gmail.com>
2025-08-22 20:14:24 +0300

    Update realcrown_app.py

--- realcrown_app.py
+++ realcrown_app.py
@@ -1,6 +1,3 @@
-from st_supabase_connection import SupabaseConnection
-conn = st.connection("supabase", type=SupabaseConnection)
-
 import streamlit as st
 import random
 from datetime import datetime
@@ -271,6 +268,7 @@ if st.button("Submit Assessment"):
     }
     response = conn.table("assessments").insert(data).execute()
     st.success("Vitals submitted to 'assessments' successfully!")
+
 
 
 
